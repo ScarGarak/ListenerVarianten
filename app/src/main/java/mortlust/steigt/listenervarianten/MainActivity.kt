74cb4250c6699665e721f35c29ea00f2cf11b11b
Mort <othummerer@gmx.de>
2023-08-18 13:39:59 +0200

    first commit

---
+++ app/src/main/java/mortlust/steigt/listenervarianten/MainActivity.kt
@@ -0,0 +1,27 @@
+package mortlust.steigt.listenervarianten
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mortlust.steigt.listenervarianten.databinding.ActivityMainBinding
+import android.view.LayoutInflater
+import android.view.View
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var B: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        B = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(B.root)
+
+        B.buOne.setOnClickListener(this)
+        B.buTwo.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            B.buOne.id -> B.tvOutput.text = getString(R.string.buOneName)
+            B.buTwo.id -> B.tvOutput.text = getString(R.string.buTwoName)
+        }
+    }
+
+}
